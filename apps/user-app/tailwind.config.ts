aa7f138c56710065e4d97d0bf0cf7d4773f6c6b3
Shimil Tharayil <109408967+shimiltharayil@users.noreply.github.com>
2024-03-30 23:18:58 +0530

    added database package

--- apps/user-app/tailwind.config.ts
+++ apps/user-app/tailwind.config.ts
@@ -2,8 +2,6 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "../../packages/ui/node_modules/**/*.{js,ts,tsx,mdx}",
 
     // Or if using `src` directory:
