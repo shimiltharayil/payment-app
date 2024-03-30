a07af5d289e3f391f826a41e643d0a125bd0a718
Shimil Tharayil <109408967+shimiltharayil@users.noreply.github.com>
2024-03-31 00:09:30 +0530

    error solved

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,5 @@
+import { PrismaClient } from "@repo/database/client";
+const client = new PrismaClient();
 
 export default function Page(): JSX.Element {
   return <div className="bg-green-500">Hello</div>;
